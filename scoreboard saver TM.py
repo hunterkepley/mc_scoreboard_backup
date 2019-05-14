94e838a29ab92c5ee0864f812dff6915bcd71172
Hunter Kepley <kepley.l.hunter@gmail.com>
2019-05-14 08:58:44 -0700

    final

--- scoreboard saver TM.py
+++ scoreboard saver TM.py
@@ -1,6 +1,6 @@
 from shutil import copyfile
-import subprocess
 import os
+import time
 
 def main():
     print("COPYIN THAT SCOREBOARD OVER INTO THE SERVER LUIGUS")
@@ -15,6 +15,19 @@ def main():
 
     print("Luigi this shit will take a minute to update, have a good day, it's loaded xd lmao hehe content lul 4head just build nice keyboard")
 
+    while(True):
+        print("Backin up the damn scoreboard mr lugi")
+        try:
+            copyfile("data/scoreboard.dat", "scoreboard_backup/scoreboard.dat.bak")
+        except:
+            try:
+                os.mkdir('scoreboard_backup')
+                copyfile("data/scoreboard.dat", "scoreboard_backup/scoreboard.dat.bak")
+            except FileExistsError:
+                print("scoreboard_backup directory exists in location 'scoreboard_backup")
+            copyfile("data/scoreboard.dat", "scoreboard_backup/scoreboard.dat.bak")
+        
+        time.sleep(300)
 
 if __name__ == "__main__":
     main()
